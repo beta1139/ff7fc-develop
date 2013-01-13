9f6845b42111181809f2a6ea36769e8560b408a5
beta1139 <beta1139@267a9591-72e6-0956-6469-16bacd8914ac>
2013-01-13 06:02:08 +0000

    未使用のマップチップ削除

--- tools/map_chip_edit/main.cpp
+++ tools/map_chip_edit/main.cpp
@@ -12,210 +12,25 @@
 #define CHIP_W (16)
 #define CHIP_H (16)
 
-#define RGB 0
-#define YUV 1
-#define HUE 2
-#define HSV 3
+#include <list>
+#include <algorithm>
 
-#define ALGORITHM HSV
+using namespace std;
 
-double rgb_to_y(UI r, UI g, UI b)  { return (0.299*r + 0.587*g + 0.114*b); }
-double rgb_to_u(UI r, UI g, UI b) { return ( -0.14713*r - 0.28886*g + 0.436*b); }
-double rgb_to_v(UI r, UI g, UI b) { return (0.615*r - 0.51499*g - 0.10001*b); }
+typedef list<UI>::iterator ITR;
 
-double get_hue(UI r, UI g, UI b) { return atan((sqrt(3.0)*(g-b)) / (2*r-g-b)); }
-
-int get_rgb_max(UI r, UI g, UI b)
+bool is_fc_col(UI col)
 {
-	UI max = r;
-	if(max < g){ max = g; }
-	if(max < b){ max = b; }
-	return max;
-}
-
-int get_rgb_min(UI r, UI g, UI b)
-{
-	UI min = r;
-	if(min > g){ min = g; }
-	if(min > b){ min = b; }
-	return min;
-}
-
-void get_hsv_from_rgb(double* h, double* s, double* v, UI r, UI g, UI b)
-{
-	int max = get_rgb_max(r, g, b);
-	int min = get_rgb_min(r, g, b);
-
-	*v = max;
-
-	if(*v == 0)
+	UI fc_col;
+	for(int i=0; i<COL_DATA_NUM; i++)
 	{
-		*s = 0;
-		*h = 0;
-	}
-	else
-	{
-		*s = 255.0*(max - min) / max;
-
-		if(max == min)
+		fc_col = (col_tbl[i].r<<16) | (col_tbl[i].g<<8) | (col_tbl[i].b);
+		if(fc_col == col)
 		{
-			*h = 0;
-		}
-		else
-		{
-			if(r == max)
-			{
-				*h = 60*(b-g) / (max - min);
-			}
-			else if(g == max)
-			{
-				*h = 60*(2 + (r - b) / (max - min));
-			}
-			else
-			{
-				*h = 60*(4 + (g - r) / (max - min));
-			}
+			return true;
 		}
 	}
-}
-
-UI search_nearest_fc_col(UI col)
-{
-#if (ALGORITHM==RGB)
-	UI org_r,org_g,org_b;
-	org_r = (col >> 16) & 0x000000ff;
-	org_g = (col >>  8) & 0x000000ff;
-	org_b = (col >>  0) & 0x000000ff;
-
-	double nearest_distance = 0x8FFFFFFF;
-	UI nearest_col_idx = 0;
-
-	UI r_dis, g_dis, b_dis;
-	UI fc_r,fc_g,fc_b;
-
-	for(int i=0; i<COL_DATA_NUM; i++){
-		fc_r = col_tbl[i].r;
-		fc_g = col_tbl[i].g;
-		fc_b = col_tbl[i].b;
-
-		r_dis = (fc_r-org_r)*(fc_r-org_r);
-		g_dis = (fc_g-org_g)*(fc_g-org_g);
-		b_dis = (fc_b-org_b)*(fc_b-org_b);
-
-		double col_distance = r_dis + g_dis + b_dis;
-		if(col_distance < nearest_distance){
-			nearest_col_idx = i;
-			nearest_distance = col_distance;
-		}
-
-		/*
-		printf("org(%02x %02x %02x) col_tbl[%02d](%02x %02x %02x) distance: %u\n",
-				org_r, org_g, org_b, i, fc_r, fc_g, fc_b, col_distance);
-				*/
-	}
-#elif (ALGORITHM==YUV)
-	UI org_r,org_g,org_b;
-	org_r = (col >> 16) & 0x000000ff;
-	org_g = (col >>  8) & 0x000000ff;
-	org_b = (col >>  0) & 0x000000ff;
-	double org_y  = rgb_to_y(org_r, org_g, org_b);
-	double org_cb = rgb_to_u(org_r, org_g, org_b);
-	double org_cr = rgb_to_v(org_r, org_g, org_b);
-
-	double nearest_distance = 0x8FFFFFFF;
-	UI nearest_col_idx = 0;
-
-	double y_dis, cb_dis, cr_dis;
-
-	UI fc_r,fc_g,fc_b;
-	double fc_y, fc_cb, fc_cr;
-
-	for(int i=0; i<COL_DATA_NUM; i++){
-		fc_r = col_tbl[i].r;
-		fc_g = col_tbl[i].g;
-		fc_b = col_tbl[i].b;
-
-		fc_y  = rgb_to_y(fc_r,  fc_g, fc_b);
-		fc_cb = rgb_to_u(fc_r, fc_g, fc_b);
-		fc_cr = rgb_to_v(fc_r, fc_g, fc_b);
-
-		y_dis  = sqrt((fc_y - org_y)*(fc_y - org_y));
-		cb_dis = sqrt((fc_cb-org_cb)*(fc_cb-org_cb));
-		cr_dis = sqrt((fc_cr-org_cr)*(fc_cr-org_cr));
-
-		double col_distance = y_dis + cb_dis + cr_dis;
-		if(col_distance < nearest_distance){
-			nearest_col_idx = i;
-			nearest_distance = col_distance;
-		}
-	}
-#elif (ALGORITHM==HUE)
-	UI org_r,org_g,org_b;
-	org_r = (col >> 16) & 0x000000ff;
-	org_g = (col >>  8) & 0x000000ff;
-	org_b = (col >>  0) & 0x000000ff;
-
-	double nearest_distance = 0x8FFFFFFF;
-	UI nearest_col_idx = 0;
-
-	double org_hue = get_hue(org_r, org_g, org_b);
-
-	UI fc_r,fc_g,fc_b;
-
-	for(int i=0; i<COL_DATA_NUM; i++){
-		fc_r = col_tbl[i].r;
-		fc_g = col_tbl[i].g;
-		fc_b = col_tbl[i].b;
-
-		double fc_hue = get_hue(fc_r, fc_g, fc_b);
-
-		double col_distance = (org_hue - fc_hue)*(org_hue - fc_hue);
-
-		if(col_distance < nearest_distance){
-			nearest_col_idx = i;
-			nearest_distance = col_distance;
-		}
-	}
-#elif (ALGORITHM==HSV)
-	UI org_r,org_g,org_b;
-	org_r = (col >> 16) & 0x000000ff;
-	org_g = (col >>  8) & 0x000000ff;
-	org_b = (col >>  0) & 0x000000ff;
-
-	double nearest_distance = 0x8FFFFFFFFFFFFFFF;
-	UI nearest_col_idx = 0;
-
-	double org_h, org_s, org_v;
-	get_hsv_from_rgb(&org_h, &org_s, &org_v, org_r, org_g, org_b);
-
-	UI fc_r,fc_g,fc_b;
-	double fc_h, fc_s, fc_v;
-	double h_dis, s_dis, v_dis;
-
-	for(int i=0; i<COL_DATA_NUM; i++){
-		fc_r = col_tbl[i].r;
-		fc_g = col_tbl[i].g;
-		fc_b = col_tbl[i].b;
-
-		get_hsv_from_rgb(&fc_h, &fc_s, &fc_v, fc_r, fc_g, fc_b);
-
-		h_dis = (fc_h-org_h)*(fc_h-org_h);
-		s_dis = (fc_s-org_s)*(fc_s-org_s);
-		v_dis = (fc_v-org_v)*(fc_v-org_v);
-
-		double col_distance = h_dis + s_dis + v_dis;
-
-		if(col_distance < nearest_distance){
-			nearest_col_idx = i;
-			nearest_distance = col_distance;
-		}
-	}
-#endif /* ALGORITHM */
-	/*
-	printf("nearest_col_idx: %u\n", nearest_col_idx);
-	exit(1);
-	*/
-	return ((col_tbl[nearest_col_idx].r<<16) + (col_tbl[nearest_col_idx].g<<8) + col_tbl[nearest_col_idx].b);
+	return false;
 }
 
 int main(void)
@@ -272,33 +87,77 @@ int main(void)
 	// draw color pallet
 	for(int z=0; z<COL_DATA_NUM; z++)
 	{
-		for(int i=0; i<2; i++)
+		for(int i=0; i<4; i++)
 		{
-			for(int j=0; j<2; j++)
+			for(int j=0; j<4; j++)
 			{
-				bmp.set_col(256-32+i+(z/4)*2, j+(z%4)*2, ((col_tbl[z].r<<16) + (col_tbl[z].g<<8) + col_tbl[z].b));
+				bmp.set_col(256-64+i+(z/4)*4, j+(z%4)*4, ((col_tbl[z].r<<16) + (col_tbl[z].g<<8) + col_tbl[z].b));
 			}
 		}
 	}
-	bmp.save_bmp("map_chip_draw_unused.bmp");
+	//bmp.save_bmp("map_chip_draw_unused.bmp");
 
-	// chg to fc color
+	list<UI> used_col_tbl;
+	ITR itr;
+
 	UI col;
-	UI nearest_col;
 	for(int y=0; y<bmp.get_h(); y++)
 	{
 		system("cls");
-		printf("redraw with fc col: %d / %d\n", y, bmp.get_h()-1);
+		printf("checking used color: %d / %d\n", y, bmp.get_h()-1);
 
 		for(int x=0; x<bmp.get_w(); x++)
 		{
 			col = bmp.get_col(x, y);
-			nearest_col = search_nearest_fc_col(col);
-			bmp.set_col(x, y, nearest_col);
+
+			itr = find(used_col_tbl.begin(), used_col_tbl.end(), col);
+			if(itr == used_col_tbl.end())
+			{
+				used_col_tbl.push_back(col);
+			}
 		}
-		//if(y==15) { break; }
 	}
-	bmp.save_bmp("map_chip_fc_col.bmp");
+
+	printf("used_col_tbl_num: %d\n", used_col_tbl.size());
+
+	int fc_col_num = 0;
+	int not_fc_col_num = 0;
+
+	int draw_pos_x = 256-4;
+	int draw_pos_y = 16;
+
+	for(itr=used_col_tbl.begin(); itr!=used_col_tbl.end(); ++itr)
+	{
+		if(is_fc_col(*itr))
+		{
+			fc_col_num++;
+		}
+		else
+		{
+			not_fc_col_num++;
+
+			for(int y=0; y<4; y++)
+			{
+				for(int x=0; x<4; x++)
+				{
+					bmp.set_col(draw_pos_x + x, draw_pos_y + y, *itr);
+				}
+			}
+			draw_pos_y += 4;
+
+			if(draw_pos_y == 16+4*4)
+			{
+				draw_pos_y = 16;
+				draw_pos_x -= 4;
+			}
+		}
+	}
+
+	printf("fc_col_num: %d\n", fc_col_num);
+	printf("not_fc_col_num: %d\n", not_fc_col_num);
+
+
+	bmp.save_bmp("map_chip_out.bmp");
 
 	return 0;
 }
